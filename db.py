91e270e9d52fc0ea4f94f44bc18ef74c1b3c6784
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2017-09-05 13:11:57 -0700

    First database access

--- db.py
+++ db.py
@@ -1,4 +1,50 @@
+class DB_LINK(object):
+
+    def __init__(self, theTable, theField, theValue, theLinkTable, theLinkRow):
+        self._linkTable = theLinkTable
+        self._linkRow = theLinkRow
+        self._table = theTable
+        self._field = theField
+        self._value = theValue
+
+
 class DB_TABLE(object):
+
+    __ID = 0
+
+    def __init__(self, theName, theFields):
+        self.id = DB_TABLE.__ID + 1
+        DB_TABLE.__ID += 1
+        self.Name = theName
+        self.Fields = theFields
+        self.Table = []
+        self.RowCb = []
+        self.TableCb = []
+        self.RowId = 0
+        self.RegisterLinks = []
+        self.Row = DB_ROW
+
+    def createRow(self, **kwargs):
+        self.RowId += 1
+        kwargs['id'] = self.RowId
+        row = self.Row()
+        for name, default in self.Fields.items():
+            setattr(row, name, default)
+        row.update(**kwargs)
+        return row
+
+    def registerLink(self, theField, theValue, theLinkTable, theLinkRow):
+        self.RegisterLinks.append(DB_LINK(self.Name, theField, theValue, theLinkTable, theLinkRow))
+
+
+class DB_ROW(object):
+
+    def __init__(self, **kwargs):
+        self.update(**kwargs)
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
     def __repr__(self):
         return "\n".join(['{0}: {1}'.format(k, v) for k, v in self.__dict__.items()])
@@ -9,41 +55,50 @@ class DB(object):
     def __init__(self):
         self._db = {}
 
-    def createTable(self, theTable, theFields, theCb=None):
-        tb = DB_TABLE
-        self._db.update({theTable: {'klass': tb, 'table': [], 'cb': theCb, 'id': 0}})
-        for name, default in theFields.items():
-            setattr(tb, name, default)
+    def createTable(self, theTable, theFields):
+        tb = DB_TABLE(theTable, theFields)
+        self._db.update({theTable: tb})
         return tb
 
+    def getTableByName(self, theTable):
+        tb = self._db[theTable]
+        return tb
+
+    def getAllTableNames(self):
+        for name in self._db.keys():
+            yield name
+
     def addRowToTable(self, theTable, **kwargs):
-        Tb = self._db[theTable]['klass']
-        row = Tb()
-        id = self._db[theTable]['id'] + 1
-        setattr(row, 'id', id)
-        self._db[theTable]['id'] = id
-        for k, v in kwargs.items():
-            setattr(row, k, v)
-        self._db[theTable]['table'].append(row)
-        if self._db[theTable]['cb']:
-            self._db[theTable]['cb'](row)
+        tb = self._db[theTable]
+        row = tb.createRow(**kwargs)
+        tb.Table.append(row)
+        for cb in tb.RowCb:
+            cb(self, tb, row)
         return row
 
-    def deleteRowFromTable(self, theTable, theId):
-        Tb = self._db[theTable]['table']
-        for index, entry in enumerate(Tb):
+    def updateRowFromTable(self, theTable, theId, **kwargs):
+        tb = self._db[theTable].Table
+        for entry in tb:
             if entry.id == theId:
                 break
-        del Tb[index]
+        entry.update(**kwargs)
+        return entry
+
+    def deleteRowFromTable(self, theTable, theId):
+        tb = self._db[theTable].Table
+        for index, entry in enumerate(tb):
+            if entry.id == theId:
+                break
+        del tb[index]
 
     def getRowFromTable(self, theTable, theId):
-        Tb = self._db[theTable]['table']
-        for index, entry in enumerate(Tb):
+        tb = self._db[theTable].Table
+        for entry in tb:
             if entry.id == theId:
                 break
-        return Tb[index]
+        return entry
 
     def getAllRowFromTable(self, theTable):
-        Tb = self._db[theTable]['table']
-        for entry in Tb:
+        tb = self._db[theTable].Table
+        for entry in tb:
             yield entry
--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import sys
+import db
 
 
 sys.path.insert(0, 'jc2li.zip')
 import jc2li.loggerator as loggerator
 logger = loggerator.getLoggerator('main')
 logger.info('MAIN APPLICATION')
+
+
+def person(db, table, row):
+    otherTable = db.getTableByName('Company')
+    for otherRow in otherTable.Table:
+        if otherRow.NAME == row.company:
+            row.active = True
+    otherTable.registerLink('NAME', row.company, table.Name, row.id)
+
+
+def company(db, table, row):
+    for link in table.RegisterLinks:
+        if getattr(row, link._field) == link._value:
+            logger.display('there is a link to table {0} row {1}'.format(link._linkTable, link._linkRow))
+            db.getRowFromTable(link._linkTable, link._linkRow).update(active=True)
+
+
+myDB = db.DB()
+myDB.createTable('Person', {'name': '---', 'age': 0, 'company': '---', 'active': False})
+myDB.createTable('Company', {'NAME': '***', 'COUNTRY': '***'})
+myDB.getTableByName('Person').RowCb.append(person)
+myDB.getTableByName('Company').RowCb.append(company)
+
+jc = myDB.addRowToTable('Person', name='Jose Carlos', age=50, company='CISCO')
+logger.display(jc)
+matias = myDB.addRowToTable('Person', name='Matias', age=50, company='CISCO')
+logger.display(matias)
+joselu = myDB.addRowToTable('Person', name='Joselu', age=50, company='NOKIA')
+logger.display(joselu)
+cisco = myDB.addRowToTable('Company', NAME='CISCO', COUNTRY='USA')
+logger.display(cisco)
+_jc = myDB.getRowFromTable('Person', jc.id)
+_matias = myDB.getRowFromTable('Person', matias.id)
+_joselu = myDB.getRowFromTable('Person', joselu.id)
+logger.display(_jc)
+logger.display(_matias)
+logger.display(_joselu)
