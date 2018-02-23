91695b9577a7f4cafc1535b6734ad918ca4c2b75
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2018-02-23 10:05:55 -0800

    Create example/cli.

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ CURRENT_DIR=`pwd`
 REPO_PATH=${HOME}/Repository
 JC2CLI_PATH=${REPO_PATH}/jc2cli
 JC2DB_PATH=${REPO_PATH}/jc2db/jc2db
+CLI_PATH=${REPO_PATH}/jc2db/examples/cli
 VENV_PATH=${HOME}/virtualenv
 P3_PATH=${VENV_PATH}/python3/bin/
 PYTHON=python
-RUN=${JC2DB_PATH}/run.py
+RUN=${CLI_PATH}/run.py
 
 export PYTHONPATH=$PYTHONPATH:.:${JC2CLI_PATH}
 
